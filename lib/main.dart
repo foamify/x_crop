afc567076ed98b364fa17428b4fabfaaed1c8034
A Arif A S <arifauliasutarman@gmail.com>
2024-01-12 07:03:36 +0700

    try fix

--- lib/main.dart
+++ lib/main.dart
@@ -158,34 +158,107 @@ class MainApp extends StatelessWidget {
                             return;
                           }
 
-                          final innerOuter = outer.innerQuad(newQuad);
+                          final currPoint = switch (i) {
+                            0 => newQuad.point0,
+                            1 => newQuad.point1,
+                            2 => newQuad.point2,
+                            _ => newQuad.point3,
+                          };
 
-                          final triPoints = outs.mapIndexed((index, e) {
-                            print((index + 2) % 4);
-                            final end1 = switch (index) {
-                              1 => newQuad.point3,
-                              2 => newQuad.point0,
-                              3 => newQuad.point1,
-                              _ => newQuad.point2,
+                          final opposingPoint = switch (i) {
+                            0 => newQuad.point2,
+                            1 => newQuad.point3,
+                            2 => newQuad.point0,
+                            _ => newQuad.point1,
+                          };
+
+                          var tripoints = outs.map((e) {
+                            final innerCurrPoint = switch (e.$1) {
+                              0 => outInner.point0,
+                              1 => outInner.point1,
+                              2 => outInner.point2,
+                              _ => outInner.point3,
                             };
-                            final end2 = switch (index) {
-                              1 => newQuad.rect.topRight,
-                              2 => newQuad.rect.bottomRight,
-                              3 => newQuad.rect.bottomLeft,
-                              _ => newQuad.rect.topLeft,
+                            final outInnerOpposite = switch (i) {
+                              0 => outInner.point2,
+                              1 => outInner.point3,
+                              2 => outInner.point0,
+                              _ => outInner.point1,
                             };
-                            final angle =
-                                getAngleFromPoints(end1.toOffset(), end2);
-                            final isYOutOfBound = true;
 
-                            return getTriangleFromLineAndTwoAngle(
-                                end2.toVector3(),
-                                outInner.points[i],
-                                angle,
-                                isYOutOfBound);
-                          });
+                            final outerEdge = switch (e.$1) {
+                              0 => outer.rect.topLeft,
+                              1 => outer.rect.topRight,
+                              2 => outer.rect.bottomRight,
+                              _ => outer.rect.bottomLeft,
+                            };
+                            print('isY:');
+                            print(innerCurrPoint.y < outer.rect.top ||
+                                innerCurrPoint.y > outer.rect.bottom);
+                            print('angle: ${getAngleFromPointsVec3(
+                              outInnerOpposite,
+                              innerCurrPoint,
+                            )}');
+                            return rotateVector(
+                              getTriangleFromLineAndTwoAngle(
+                                outInnerOpposite,
+                                outerEdge.toVector3(),
+                                getAngleFromPointsVec3(
+                                  outInnerOpposite,
+                                  innerCurrPoint,
+                                ),
+                                innerCurrPoint.y > outer.rect.top ||
+                                    innerCurrPoint.y < outer.rect.bottom,
+                              ),
+                              outer.rect.center.toVector3(),
+                              outer.angle,
+                            );
+                          }).toList();
 
-                          debugPoints.value = [...triPoints];
+                          final snapPoint = tripoints.first;
+
+                          print(snapPoint);
+
+                          debugPoints.value = [
+                            // outer.rect.topLeft.toVector3(),
+                            // outer.rect.topRight.toVector3(),
+                            // outer.rect.bottomRight.toVector3(),
+                            // outer.rect.bottomLeft.toVector3(),
+                            // ...outer.innerQuad(newQuad).points,
+                            ...tripoints,
+                            // tripoints[outClosest.$1],
+                            ...outs.map((e) => e.$2),
+                            // innerCurrPoint
+                          ];
+
+                          switch (i) {
+                            case 0:
+                              point0 = snapPointToLine(initialQuad().point0,
+                                  initialQuad().point2, snapPoint);
+                            case 1:
+                              point1 = snapPointToLine(initialQuad().point1,
+                                  initialQuad().point3, snapPoint);
+                            case 2:
+                              point2 = snapPointToLine(initialQuad().point2,
+                                  initialQuad().point0, snapPoint);
+                            case 3:
+                              point3 = snapPointToLine(initialQuad().point3,
+                                  initialQuad().point1, snapPoint);
+                          }
+
+                          newQuad = i == 0 || i == 2
+                              ? QuadUtils.fromPoints02(
+                                  point0,
+                                  point2,
+                                  initialQuad().rect.center.toVector3(),
+                                  initialQuad().angle,
+                                )
+                              : QuadUtils.fromPoints13(
+                                  point1,
+                                  point3,
+                                  initialQuad().rect.center.toVector3(),
+                                  initialQuad().angle,
+                                );
 
                           innerQuad.value = newQuad;
                         },
